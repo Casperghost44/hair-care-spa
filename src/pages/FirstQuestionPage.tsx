3d4ac631dc61d0ae19bcdafd2fbc80757037b7a8
bratzabratalee <ahsb.emeshcheryakova@gmail.com>
2024-10-11 08:21:25 +0300

    added data for questions

--- src/pages/FirstQuestionPage.tsx
+++ src/pages/FirstQuestionPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const FirstQuestionPage = () => {
-  return <div>FirstQuestionPage</div>;
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-[#1C2635] font-medium text-3xl text-center">
+          What's your hair type or texture?
+        </h1>
+        <div className="grid grid-cols-4 gap-x-2">
+          <div className="w-full border-1 border-solid border-[#5BC1ED] rounded-lg"></div>
+          <div className="w-full border-1 border-solid border-[#5BC1ED] rounded-lg"></div>
+          <div className="w-full border-1 border-solid border-[#5BC1ED] rounded-lg"></div>
+          <div className="w-full border-1 border-solid border-[#5BC1ED] rounded-lg"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FirstQuestionPage;
